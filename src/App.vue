4c830ff7b72ae6978744c04a3522f82b6c28cbbf
Ari <ari@llamanet.de>
2022-01-07 21:25:41 +0100

    Add Rubik font

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,26 @@
 </template>
 
 <script>
-import TitleBar from './components/TitleBar.vue';
+    import TitleBar from './components/TitleBar.vue';
+    import '@fontsource/rubik';
 
-export default {
-  name: 'App',
-  components: {
-    TitleBar
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            TitleBar
+        }
+    }
 </script>
 
 <style lang="scss">
-@import 'styles/variables.scss';
-body{ margin: 0; background-color: $window-background;}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  background-color: $window-background;
-}
+    @import 'styles/variables.scss';
+    body{ margin: 0; background-color: $window-background;}
+    #app {
+        font-family: 'Rubik', Arial, Helvetica, sans-serif;
+        color: $foreground-100;
+        font-size: 14px;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        background-color: $window-background;
+    }
 </style>
