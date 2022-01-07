981035ed19d5698e3d92f2953e4f43f8b5ada695
Ari <ari@llamanet.de>
2022-01-07 21:25:32 +0100

    Add title bar icons and search area

--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -5,36 +5,53 @@
             <div id="min"></div>
             <div id="max"></div>
         </div>
+        <SearchBar placeholder="Search for loops..."></SearchBar>
+        <div class="actions">
+            <vue-feather type="folder" size="18"></vue-feather>
+            <vue-feather type="settings" size="18"></vue-feather>
+        </div>
     </div>
 </template>
 
 <script>
-  export default {};
+    import VueFeather from 'vue-feather';
+    import SearchBar from './inputs/SearchBar';
+    export default {
+        components: {
+            VueFeather,
+            SearchBar
+        }
+    };
 </script>
 
 <style lang="scss">
     @import '../styles/variables.scss';
+    @import '../styles/icons.scss';
     .title-bar{
         -webkit-app-region: drag;
-        background-color: $background-100;
+        justify-content: space-between;
+        background-color: $titlebar-background;
         color: $foreground-100;
         padding: 0 21px;
         height: 52px;
         display: flex;
         align-items: center;
+        gap: $titlebar-gap;
+        >*{
+            -webkit-app-region: no-drag;
+        }
     }
     .window-btns {
         display: flex;
         gap: 8px;
         div{
-            -webkit-app-region: no-drag;
             width: 12px;
             height: 12px;
-            background-color: $foreground-100;
+            background-color: $foreground-200;
             border-radius: 6px;
         }
         div:active{
-            background-color: $foreground-200 !important;
+            background-color: $foreground-100 !important;
         }
         #close:hover{
             background-color: $accent-1;
@@ -44,6 +61,15 @@
         }
         #min:hover{
             background-color: $accent-3;
+        }
+    }
+    .actions{
+        display: flex;
+        gap: $titlebar-gap;
+        height: 18px;
+        svg{
+            stroke: $foreground-200;
+            stroke-width: 2.5;
         }
     }
 </style>
---
+++ src/components/inputs/SearchBar.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="SearchBar" ref="SearchBar" onclick="document.querySelector('.input').focus()">
+        <div class="inner">
+            <vue-feather type="search" size="12" class="search"></vue-feather>
+            <input class="input"
+                ref="input"
+                contenteditable="true"
+                @focusout="onUnfocus"
+                @focus="onFocus"
+                @keypress="resizeInput"
+                v-bind:placeholder="placeholder"
+            >
+        </div>
+    </div>
+</template>
+
+<script>
+    import VueFeather from 'vue-feather';
+    export default {
+        name: 'SearchBar',
+        components: {
+            VueFeather
+        },
+        props: {
+            placeholder: String
+        },
+        methods: {
+            onUnfocus(){
+                this.$refs.SearchBar.className = "SearchBar";
+                if((this.$refs.input.value).length == 0) this.$refs.input.style.width = '';
+                else this.$refs.input.style.width = (((this.$refs.input.value).length) + 1) + 'ch';
+            },
+            onFocus(){
+                this.$refs.SearchBar.className = "SearchBar active";
+            },
+            resizeInput(){
+                if((this.$refs.input.value).length == 0) this.$refs.input.style.width = '';
+                else this.$refs.input.style.width = (((this.$refs.input.value).length) + 1) + 'ch';
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+@import "../../styles/variables.scss";
+
+[contentEditable=true]:not(:focus):before{
+    content:attr(data-placeholder);
+}
+
+::-webkit-input-placeholder{
+    color: $foreground-200;
+}
+
+.SearchBar{
+    flex-shrink: 1;
+    flex-grow: 0;
+    width: 437px;
+    .inner svg{
+        stroke: $foreground-200;
+        stroke-width: 3.5;
+    }
+    background-color: $background-200;
+    border-radius: $radius;
+    height: 26px;
+    transition-duration: $animation-duration;
+    transition-timing-function: $animation-timing;
+    cursor: text;
+}
+.SearchBar.active{
+    .inner{
+        margin-left: $titlebar-gap;
+        transform: translateX(0);
+        svg{
+            stroke: $foreground-100;
+        }
+    }
+    .input{
+        color: $foreground-100;
+    }
+}
+.inner{
+    display: flex;
+    align-items: center;
+    gap: 6px;
+    margin-left: 50%;
+    height: 100%;
+    transform: translateX(-50%);
+    width: min-content;
+    min-width: 20px;
+    transition-duration: $animation-duration;
+    transition-timing-function: $animation-timing;
+}
+.input{
+    background: transparent;
+    border: 0px;
+    outline: 0px;
+    outline: none;
+    display: inline-block;
+    white-space: nowrap;
+    color: $foreground-200;
+    overflow: visible;
+    width: 110px;
+    padding: 0;
+    margin: 0;
+}
+</style>
