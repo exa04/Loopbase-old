ed43603e5aa242a9cb9b7fe5ee5261062adfcbc2
Ari <ari@llamanet.de>
2022-01-09 11:56:29 +0100

    Add IPC hooks

---
+++ src/preload.js
@@ -0,0 +1,11 @@
+const {ipcRenderer, contextBridge} = require('electron');
+
+contextBridge.exposeInMainWorld("ipcRenderer",ipcRenderer);
+
+contextBridge.exposeInMainWorld('electron', {
+  startDrag: (fileName) => {
+    ipcRenderer.send('ondragstart', fileName)
+  }
+})
+
+alert("hi");
