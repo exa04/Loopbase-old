6b67532b9b0e4a99fe0a6f0c2019167d37bedf13
stxarixdust <ari@llamanet.de>
2022-03-25 10:47:13 +0100

    Add Select element

---
+++ src/components/inputs/Select.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <div class="select-layout">
+            <div class="title"><slot/></div>
+            <Button
+                secondary
+                class="il-container centered select-btn"
+                @click="open = !open"
+                :class="{expanded: open}"
+            >
+                <span >{{
+                    options.find(o => o[0] == selected)[1]
+                }}</span>
+                <VueFeather
+                    :type="'chevron-down'"
+                    :class="{rotated180: open}"
+                    class="select-chevron"
+                />
+            </Button>
+        </div>
+        <Transition name="select-popup">
+            <div class="select-container" v-if="open">
+                <div class="select">
+                    <div
+                        class="option"
+                        v-for="o in options"
+                        :key="o[0]"
+                        :class="{selected : o[0] == selected}"
+                        @click="this.$emit('selectOption', o[0]);"
+                    >
+                        {{o[1]}}
+                    </div>
+                </div>
+            </div>
+        </Transition>
+    </div>
+</template>
+
+<script>
+import Button from './Button.vue';
+import VueFeather from 'vue-feather';
+
+export default {
+    name: "Select",
+    components: {
+        Button,
+        VueFeather
+    },
+    data() {
+        return{
+            open: true
+        }
+    },
+    props: {
+        options: {
+            type: Array,
+            required: true
+        },
+        selected: String
+    }
+}
+</script>
