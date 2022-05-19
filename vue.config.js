a8200bbed1c2f668a85318e6aaec8efb4f9ef4e7
stxarixdust <ari@llamanet.de>
2022-05-19 10:40:26 +0200

    Update deps and add prettier

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,23 @@
-module.exports = {
-    pluginOptions: {
-        nodeIntegration: true,
-        electronBuilder: {
-            nodeIntegration: true,
-            preload: 'src/preload.js',
-            builderOptions: {
-              win: {
-                icon: './public/icons/win/icon.ico'
-              },
-              mac: {
-                category: 'public.app-category.music',
-                icon: './public/icons/mac/icon.icns'
-              },
-              linux: {
-                icon: './public/icons/png',
-                category: 'Audio'
-              },
-            }
-        }
-    }
-}
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  pluginOptions: {
+    nodeIntegration: true,
+    electronBuilder: {
+      nodeIntegration: true,
+      preload: "src/preload.js",
+      builderOptions: {
+        win: {
+          icon: "./public/icons/win/icon.ico",
+        },
+        mac: {
+          category: "public.app-category.music",
+          icon: "./public/icons/mac/icon.icns",
+        },
+        linux: {
+          icon: "./public/icons/png",
+          category: "Audio",
+        },
+      },
+    },
+  },
+});
